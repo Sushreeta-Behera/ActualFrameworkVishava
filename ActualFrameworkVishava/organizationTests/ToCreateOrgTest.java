d2902a1781947a120df6561d7b31d1c07a360343
visha <visha@Vishava>
2025-06-25 15:58:02 +0530

    Fourth Push

---
+++ ActualFrameworkVishava/organizationTests/ToCreateOrgTest.java
@@ -0,0 +1,11 @@
+package organizationTests;
+
+public class ToCreateOrgTest {
+
+	public static void main(String[] args) {
+
+		System.out.println(" Organization created By Ashutosh");
+	}
+
+}
+  
---
+++ ActualFrameworkVishava/src/test/java/contactTests/ToCreateContactTest.java
@@ -0,0 +1,10 @@
+package contactTests;
+
+public class ToCreateContactTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("Contact is created By ashutosh");
+	}
+
+}
